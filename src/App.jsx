35e80fd027fa0c263d959b14068b23f68b9b6ac0
Kunal Baghel <Kunalbaghel995@gmail.com>
2024-05-30 14:27:28 +0530

    url is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import NavBar from "./components/NavBar"
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import OurTeam from "./Pages/OurTeam";
 import ContactUs from "./Pages/ContactUs";
 
 function App() {
   return (
     <>
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/assistai/" element={<Home/>} />
-          <Route path="/assistai/about-us" element={<AboutUs/>} />
-          <Route path="/assistai/contact-us" element={<ContactUs/>} />
-          <Route path="/assistai/our-team" element={<OurTeam/>} />
-        </Routes>
-      </Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/contact-us" element={<ContactUs/>} />
+        <Route path="/our-team" element={<OurTeam/>} />
+      </Routes>
     </>
   )
 }
